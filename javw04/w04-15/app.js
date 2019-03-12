297ed32f222f810a914616fd1ac08ec447b8118c
kevin89330 <43176098+kevin89330@users.noreply.github.com>
2019-03-12 21:52:47 +0800

    Add files via upload

---
+++ javw04/w04-15/app.js
@@ -0,0 +1,30 @@
+// WINDOW METHODS / OBJECTS / PROPERTIES
+
+// Alert
+//alert('hello world');
+// Prompt
+//const input = prompt('Enter name');
+//alert(input);
+// Confirm
+//if(confirm('are you sure?')){
+   // console.log('yes');
+//}else{
+   // console.log('no');
+//}
+// Location Object
+console.log(window.location);
+console.log(window.location.hostname);
+console.log(window.location.href);
+window.location.href = 'http://youtube.com';
+// Redirect
+//window.location.href = 'http://google.com';
+// History Object
+window.history.go(-1);
+
+// Navigator Object
+console.log(window.navigator);
+console.log(window.navigator.appCodeName);
+console.log(window.navigator.userAgent);
+console.log(window.navigator.platform);
+
+
